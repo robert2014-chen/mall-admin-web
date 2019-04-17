611746a44b057202d5b03c4799cbba4f55828eca
robert <robert.qiang.chen@gmail.com>
2019-04-17 17:56:29 +0800

    【1】

--- src/views/user/account/compotents/person.vue
+++ src/views/user/account/compotents/person.vue
@@ -1,10 +1,20 @@
 <template> 
-  <el-card class="form-container" shadow="never">
-    <el-steps :active="active" finishStatus="success" alignCenter="center">
-      <el-step title="个人基本信息"></el-step>
-      <el-step title="个人联系信息"></el-step>
-      <el-step title="个人认证信息"></el-step>
-    </el-steps>
-    <person-info-detail v-show="showStatus[0]" v-model="person" @nextStep="nextStep"></person-info-detail>
-  </el-card>
+  <div class="detail-container" v-show="showStatus[0]">
+    <el-card shadow="never" style="margin-top: 15px">
+      <div class="operate-container">
+      </div>
+      <div style="margin-top: 20px;border-bottom:1px solid #ebeef5;padding-bottom:5px;">
+        <svg-icon icon-class="marker" style="color: #606266"></svg-icon>
+        <span class="font-small">基本信息</span>
+      </div>
+      <div class="table-layout">
+        <el-row>
+          <el-col :span="4" class="table-cell-title">编号</el-col>
+          <el-col :span="4" class="table-cell">{{account.sn}}</el-col>
+          <el-col :span="4" class="table-cell-title">昵称</el-col>
+          <el-col :span="4" class="table-cell">{{account.nickName}}</el-col>
+        </el-row>
+      </div>
+    </el-card>
+  </div>
 </template>
